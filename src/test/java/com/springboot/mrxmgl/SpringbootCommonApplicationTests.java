ef5954afa1d95b0932838e29e531c8a763087d27
xmlmm <1050434850@qq.com>
2019-12-10 09:34:39 +0800

    init

---
+++ src/test/java/com/springboot/mrxmgl/SpringbootCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mrxmgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
